ecd2df45ff9e358e0a138687700a3c44824a4b06
Laura Hope <ssteamlaura@gmail.com>
2023-03-28 13:52:32 -0700

    Add pseudocode and separation of logic to directory.

--- README.md
+++ README.md
@@ -70,6 +70,16 @@ const text = "Red! Red. I like red, green, and yellow.";
 const word = "Red";
 numberOfOccurrencesInText(word, text);
 Expected Output: 3
+
+Test: "If an empty string is passed in as a word, it should return 0."
+Code:
+if (word.trim().length === 0) {
+  return 0;
+}
+const word = "";
+const text = "red RED Red!";
+numberOfOccurrencesInText(word, text);
+Expected Output: 0
 _________________________________________________
 
 Describe: omitOffensiveWord()
@@ -159,12 +169,74 @@ const word =
 omitOffensiveWord();
 Expected Output: "hello "
 
-RETURNS UNDEFINED
-function wordTest(stringParam) {
-  const badWordArray = ["zoinks","mupp"];
-  badWordArray.forEach(function(element){
-    if (stringParam.includes(element)){
-      stringParam.replace(element," ");
-    }
-  })
+__________________________
+Describe: boldPassage()
+
+Test: "It should return null if no word or text is entered."
+Code: function boldPassage(word, text) {
+  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+    return null;
+  }
 }
+const text = "";
+const word = "";
+boldPassage(word, text);
+Expected Output: null
+
+Test: "It should return a non-matching word in a p tag."
+Code: function boldPassage(word, text) {
+  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+    return null;
+  }
+  const p = document.createElement("p");
+  p.append(text);
+  return p;
+}
+const word = "hello";
+const text = "yo";
+boldPassage(word, text);
+Expected Output: <p>yo</p>
+
+Test: "It should return a matching word in a strong tag."
+Code: function boldPassage(word, text) {
+  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+    return null;
+  }
+  const p = document.createElement("p");
+  if (word === text) {
+    const bold = document.createElement("strong");
+    bold.append(text);
+    p.append(bold);
+  } else {
+    p.append(text);
+  }
+  return p;
+}
+const word = "hello";
+const text = "hello";
+boldPassage(word, text);
+Expected Output: <p><strong>hello</strong></p>
+
+Test: "It should wrap words that match in strong tags but not words that don't."
+Code: function boldPassage(word, text) {
+  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+    return null;
+  }
+  const p = document.createElement("p");
+  let textArray = text.split(" ");
+  textArray.forEach(function(element) {
+    if (word === element) {
+      const bold = document.createElement("strong");
+      bold.append(element);
+      p.append(bold);
+    } else {
+      p.append(element);
+    }
+    p.append(" ");
+  });
+  return p;
+}
+const word = "hello";
+const text = "hello there";
+boldPassage(word, text);
+Expected Output: <p><strong>hello</strong> there</p>
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,91 @@
+// Utility Logic
+
+function isEmpty(testString) {
+  return (testString.trim().length === 0);
+}
+
+// Business Logic
+
+function wordCounter(text) {
+  if (isEmpty(text)) {
+    return 0;
+  }
+  let wordCount = 0;
+  const textArray = text.split(" ");
+  textArray.forEach(function(element) {
+    if (!Number(element)) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+
+function numberOfOccurrencesInText(word, text) {
+  if (isEmpty(word)) {
+    return 0;
+  }
+  const textArray = text.split(" ");
+  let wordCount = 0;
+  textArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+
+// UI Logic
+
+function boldPassage(word, text) {
+  if (isEmpty(word) || isEmpty(text)) {
+    return null;
+  }
+  const p = document.createElement("p");
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (word === element) {
+      const bold = document.createElement("strong");
+      bold.append(element);
+      p.append(bold);
+    } else {
+      p.append(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      p.append(" ");
+    }
+  });
+  return p;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+  let boldedPassage = boldPassage(word, passage);
+  if (boldedPassage) {
+    document.querySelector("div#bolded-passage").append(boldedPassage);
+  } else {
+    document.querySelector("div#bolded-passage").innerText = null;
+  }
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
+
+
+
+
+
+
+
+
+
+
 // Business Logic -- good idea to set this up first
 //if empty string, should return 0 // trim takes all the whitespace from both ends of a string
 // starts at zero so the first word is "1" -- uses let because it will change
@@ -7,45 +95,91 @@
 //Number() function returns a number, so if it's NaN it will be counted in the word count
 //changed the callback function's parameter to element because it might not always be a word
 
-function wordCounter(text) {
-  if (text.trim().length === 0) { 
-    return 0;
-  }
-  let wordCount = 0; 
-  const textArray = text.split(" "); 
-  textArray.forEach(function(element) { 
-    if (!Number(element)){
-      wordCount++; 
-    }
-  });
-  return wordCount; 
-}
-// needs 2 parameters - one for the word we're looking for, and one for the whole text itself
-function numberOfOccurrencesInText(word, text) {
-  const textArray = text.split(" ");
-  let wordCount = 0;
-  textArray.forEach(function(element) {
-    if (element.toLowerCase().includes(word.toLowerCase())) {
-      wordCount++;
-    }
-  });
-  return wordCount; 
-}
+// function wordCounter(text) {
+//   if (text.trim().length === 0) { 
+//     return 0;
+//   }
+//   let wordCount = 0; 
+//   const textArray = text.split(" "); 
+//   textArray.forEach(function(element) { 
+//     if (!Number(element)){
+//       wordCount++; 
+//     }
+//   });
+//   return wordCount; 
+// }
+// // needs 2 parameters - one for the word we're looking for, and one for the whole text itself
+// function numberOfOccurrencesInText(word, text) {
+//   if (word.trim().length === 0) {
+//     return 0;
+//   }
+//   const textArray = text.split(" ");
+//   let wordCount = 0;
+//   textArray.forEach(function(element) {
+//     if (element.toLowerCase().includes(word.toLowerCase())) {
+//       wordCount++;
+//     }
+//   });
+//   return wordCount; 
+// }
 
 
-function omitOffensiveWords(text) {
-  let textArray = text.split(" ");
-  let badWords = ['zoinks', 'muppeteer', 'biffaroni', 'loopdaloop'];
-  let badString = badWords.toString();
-  let returnArray = [];
+// function omitOffensiveWords(text) {
+//   let textArray = text.split(" ");
+//   let badWords = ['zoinks', 'muppeteer', 'biffaroni', 'loopdaloop'];
+//   let badString = badWords.toString();
+//   let returnArray = [];
 
-  textArray.forEach(element => {
-    if (badString.toLowerCase().includes(element.toLowerCase())) {
-      returnArray.push("******");
-    } else {
-      returnArray.push(element);
-    }
-  });
-  return returnArray.toString(' ').replaceAll(',', ' '); 
-}
+//   textArray.forEach(element => {
+//     if (badString.toLowerCase().includes(element.toLowerCase())) {
+//       returnArray.push("******");
+//     } else {
+//       returnArray.push(element);
+//     }
+//   });
+//   return returnArray.toString(' ').replaceAll(',', ' '); 
+// }
 
+// function boldPassage(word, text) {
+//   if ((text.trim().length === 0) || (word.trim().length === 0)) {
+//     return null;
+//   }
+//   const p = document.createElement("p");
+//   let textArray = text.split(" ");
+//   textArray.forEach(function(element, index) {
+//     if (word === element) {
+//       const bold = document.createElement("strong");
+//       bold.append(element);
+//       p.append(bold);
+//     } else {
+//       p.append(element);
+//     }
+//     if (index !== (textArray.length - 1)) {
+//       p.append(" ");
+//     }
+//   });
+//   return p;
+// }
+
+// // UI LOGIC
+// // all of the code that responds to the form submission into its own UI function called handleFormSubmission()
+// function handleFormSubmission() {
+//   event.preventDefault();
+//   const passage = document.getElementById("text-passage").value;
+//   const word = document.getElementById("word").value;
+//   const wordCount = wordCounter(passage);
+//   const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+//   document.getElementById("total-count").innerText = wordCount;
+//   document.getElementById("selected-count").innerText = occurrencesOfWord;
+//   // new lines here!
+//   let boldedPassage = boldPassage(word, passage);
+//   if (boldedPassage) {
+//     document.querySelector("div#bolded-passage").append(boldedPassage);
+//   } else {
+//     document.querySelector("div#bolded-passage").innerText = null;
+//   }
+// }
+
+// window.addEventListener("load", function() {
+//   document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+// });
